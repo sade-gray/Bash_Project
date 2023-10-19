f14633278d19c0d072b104900c9eb963831886e3
Drblue500 <David.Lohan15@outlook.com>
2023-10-19 14:05:21 +0100

    Update add_friend.sh

---
+++ add_friend.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+echo "Usage: $0 <id> <friend>"
+exit 1
+fi
+
+id="$1"
+friend="$2"
+
+if [ ! -d "$id" ]; then
+echo "Error: User 'id' does not exist."
+exit 1
+fi
+
+if [ ! -d "$friend" ]; then
+echo "Error: User 'friend' does not exist."
+exit 1
+fi
+
+if  grep -q "$friend" "$id/friends.txt"; then
+echo "Error: '$friend' is already a friend of '$id'."
+exit 1
+fi
+
+echo  "$friend" >> "$id/friends.txt"
+echo "friend '$friend' added to the list of friends for user '$id'."
