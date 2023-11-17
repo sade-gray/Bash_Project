7e5e9e15477ff66b5abf2633743ed285444d4f8b
Sade Gray <129172382+sade-gray@users.noreply.github.com>
2023-11-17 18:59:45 +0200

    Update display_walls.sh

--- display_walls.sh
+++ display_walls.sh
@@ -1,43 +1,26 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-# echo "Error, program requires 3 args"
-  exit 5
+if [ "$#" -ne 1 ]; then
+    exit 1
 fi
 
-sender="$1"
-receiver="$2"
-message="$3"
+wall="$1"
 
-./acquire.sh "post_message" #only one person can acquire lock to friend files at a time
+./acquire.sh "display_wall" #only one person can acquire lock to friend files at a time
 	check=$?
         if [ $check -eq 1 ]; then
-         exit 4
+         exit 3
         fi
         if [ $check -eq 2 ]; then
-         exit 4
+         exit 3
         fi
 
-if [ ! -d "$receiver" ]; then
-#  echo "Sorry, this friend could not be found"
-./release.sh "post_message"
-  exit 1
+if [ ! -d "$wall" ]; then
+#    echo "Error, this person's wall does not exist"
+    ./release.sh "display_wall"
+    return 2
 fi
 
-if [ ! -d "$sender" ]; then
-#  echo "Sorry, this sender could not be found"
-./release.sh "post_message"
-  exit 2
-fi
+# more "$wall/wall.txt"
 
-if ! grep -q "$sender" "$receiver/friends.txt"; then
-#  echo "Sorry, you are not friends, so this message could not be posted"
-./release.sh "post_message"
-  exit 3
-fi
-
-echo "$sender: $message" >> "$receiver/wall.txt"
-./release.sh "post_message"
-
-# echo "Your message has been posted to $receiver's wall"
 exit 0
