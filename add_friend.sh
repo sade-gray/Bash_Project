69f37774565a5a03699d4fc184cf2a6744030939
Sade Gray <129172382+sade-gray@users.noreply.github.com>
2023-11-17 18:55:32 +0200

    Update add_friend.sh

--- add_friend.sh
+++ add_friend.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-./acquire.sh server.sh friendslocklock.txt #only one person can acquire lock to friend files at a time
-	./acquire.sh "$id"/friends.txt "$id"friendslock.txt
-	if ! [ $id == $friend ]; then
-		./acquire.sh "$friend"/friends.txt "$friend"friendslock.txt
-	fi
-	./release.sh friendslocklock.txt
-
 if [ "$#" -ne 2 ]; then
 # echo "Usage: $0 <id> <friend>"
-./release.sh friendslocklock.txt
-exit 0
+./release.sh "add_friends"
+#added a 1 here. will change this soon
+exit 1
 fi
 
 id="$1"
@@ -18,22 +12,32 @@ friend="$2"
 
 if [ ! -d "$id" ];then
 # echo "Error: User 'id' does not exist."
-./release.sh friendslocklock.txt
+./release.sh "add_friends"
 exit 1
 fi
 
 if [ ! -d "$friend" ];then
 # echo "Error: User 'friend' does not exist."
-./release.sh friendslocklock.txt
+./release.sh "add_friends"
 exit 2
 fi
 
 if  grep -q "$friend" "$id/friends.txt"; then
 # echo "Error: '$friend' is already a friend of '$id'."
-./release.sh friendslocklock.txt
+./release.sh "add_friends"
 exit 3
 fi
 
+./acquire.sh "add_friends" #only one person can acquire lock to friend files at a time
+	check=$?
+        if [ $check -eq 1 ]; then
+         exit 4
+        fi
+        if [ $check -eq 2 ]; then
+         exit 4
+        fi
+
 echo "$friend" >> "$id/friends.txt"
 # echo "friend '$friend' added to the list of friends for user '$id'."
-./release.sh friendslocklock.txt
+./release.sh "add_friends"
+exit 0
