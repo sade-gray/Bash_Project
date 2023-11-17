b7fc8bc902645588d3622353011687e6140dc8b5
Sade Gray <129172382+sade-gray@users.noreply.github.com>
2023-11-17 18:57:37 +0200

    Update display_walls.sh

--- display_walls.sh
+++ display_walls.sh
@@ -6,8 +6,18 @@ fi
 
 wall="$1"
 
+./acquire.sh "display_wall" #only one person can acquire lock to friend files at a time
+	check=$?
+        if [ $check -eq 1 ]; then
+         exit 3
+        fi
+        if [ $check -eq 2 ]; then
+         exit 3
+        fi
+
 if [ ! -d "$wall" ]; then
 #    echo "Error, this person's wall does not exist"
+    ./release.sh "display_wall"
     return 2
 fi
 
