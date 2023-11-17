58baf81f7494018dfe843edfe36db1c7d9684670
Sade Gray <129172382+sade-gray@users.noreply.github.com>
2023-11-17 18:56:52 +0200

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # echo "$#"
 
-./acquire.sh "$user" "$user"lock
-
 if [ "$#" -ne 1 ]; then
 echo "Usage: $0 <id>"
-./release.sh "$user"lock
 exit 0
 fi
 
@@ -13,7 +10,6 @@ id="$1"
 # echo "$id"
 if [ -d "$id" ]; then
 # echo "Error: Directory '$id' already exists."
-./release.sh "$user"lock
 exit 1
 fi
 # echo "$id"
@@ -21,6 +17,6 @@ mkdir "$id"
 
 touch "$id/wall.txt"
 touch "$id/friends.txt"
-./release.sh "$user"lock
+
 
 # echo "Directory '$id' and files 'wall.txt' and 'friends.txt' created successfully."
